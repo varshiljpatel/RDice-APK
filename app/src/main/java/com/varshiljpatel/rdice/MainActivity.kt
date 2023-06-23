fc2571259f31cd9da4af81744e8bb87e92fda829
varshilj.patel <mail.varshil@gmail.com>
2023-06-24 00:14:26 +0530

    v0.2

--- app/src/main/java/com/varshiljpatel/rdice/MainActivity.kt
+++ app/src/main/java/com/varshiljpatel/rdice/MainActivity.kt
@@ -1,31 +1,47 @@
 package com.varshiljpatel.rdice
 
 import android.annotation.SuppressLint
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.Handler
-import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import com.varshiljpatel.rdice.databinding.ActivityMainBinding
 import java.util.Random
 import kotlin.properties.Delegates
+import android.os.Vibrator
+import android.os.VibrationEffect
 
+@SuppressLint("ServiceCast")
 class MainActivity : AppCompatActivity() {
     private var randomInt by Delegates.notNull<Int>()
     private var flag : Boolean = true
+    @RequiresApi(Build.VERSION_CODES.S)
 
     // Data Binding
-    private lateinit var binding: ActivityMainBinding
-    @SuppressLint("SetTextI18n")
+    private lateinit var binding : ActivityMainBinding
+    @RequiresApi(Build.VERSION_CODES.S)
+    @SuppressLint("SetTextI18n", "ServiceCast")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val vibrator = this.getSystemService(VIBRATOR_SERVICE) as Vibrator
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.rollBtn.setOnClickListener {
             if (flag) {
+
+                if (Build.VERSION.SDK_INT >= 26) {
+                    vibrator.vibrate(VibrationEffect.createOneShot(100, VibrationEffect.DEFAULT_AMPLITUDE))
+                } else {
+                    vibrator.vibrate(100)
+                }
+
                 flag = false
                 binding.diceImg.setImageResource(R.drawable.dice0)
                 binding.digitTxt.text = "..."
+
+                // Handler
                 Handler().postDelayed({
                     try {
                         randomInt = Random().nextInt(6) + 1
@@ -53,4 +69,3 @@ class MainActivity : AppCompatActivity() {
     }
 
 }
-
--- app/src/main/java/com/varshiljpatel/rdice/Splash.kt
+++ app/src/main/java/com/varshiljpatel/rdice/Splash.kt
@@ -15,6 +15,7 @@ class Splash : AppCompatActivity() {
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+            finish()
         }, 3000)
     }
 }
